0ed3458a73cd86b7b712567ebcf086d29d2e3ec4
Meddauna <47582180+Meddauna@users.noreply.github.com>
2019-02-20 09:04:43 -0500

    entered my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a list of functions to store/get a matrix and it's inverse. 
+## USAGE: make the cache matrix with your matrix and store it as a list as follows:
+## CacheList <- makeCacheMatrix(YourMatrix)
+##
+## Call cacheSolve to calculate the inverse of your matrix if it has never been done
+## or pull the inverse from the cache if it has already been calculated:
+## cacheSolve(CacheList)
 
-## Write a short comment describing this function
+## This function creates a list of functions with your matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mx = matrix()) {
+  inv <- NULL
+  setmatrix <- function(y) {
+    mx <<- y
+    inv <<- NULL
+  }
+  getmatrix <- function() mx
+  setinverse <- function(inve) inv <<- inve
+  getinverse <- function() inv
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates a matrix inverse or pulls it from cache if it's already been calculated
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mx, ...) {
+  inv <- mx$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- mx$getmatrix()
+  inv <- solve(data)
+  mx$setinverse(inv)
+  inv
 }
